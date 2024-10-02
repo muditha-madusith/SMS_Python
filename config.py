3e6494b540a9ee64e2e1b6da1728e067b66fdbbd
Muditha Madusith <108977790+muditha-madusith@users.noreply.github.com>
2024-10-02 16:38:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 MYSQL = {
     'user': 'admin',
     'password': '54322016',
-    'host': 'database-prod.c92a0wmu8szj.eu-north-1.rds.amazonaws.com',
-    'database': 'sms_system'
+    'host': 'sms-system-test.c92a0wmu8szj.eu-north-1.rds.amazonaws.com',
+    'database': 'sms_system_test'
 }
